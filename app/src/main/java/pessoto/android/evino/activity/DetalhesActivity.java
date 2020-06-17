c2ebc21fdc10eb70411ff191174db8337acd0e16
Daniel Pessoto <d.pessoto@hotmail.com>
2020-06-17 19:12:33 -0300

    Recebendo dados de outra activity

--- app/src/main/java/pessoto/android/evino/activity/DetalhesActivity.java
+++ app/src/main/java/pessoto/android/evino/activity/DetalhesActivity.java
@@ -3,14 +3,32 @@ package pessoto.android.evino.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import pessoto.android.evino.R;
 
 public class DetalhesActivity extends AppCompatActivity {
 
+    private TextView textNomeVinho, textDescricao;
+    private ImageView imageVinho;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes);
+
+        textNomeVinho = findViewById(R.id.textNomeVinho);
+        textDescricao = findViewById(R.id.textDescricao);
+        imageVinho = findViewById(R.id.imageVinho);
+
+        //Recuperar os dados
+        Bundle dados = getIntent().getExtras();
+        String nomeVinho = dados.getString("nome");
+
+        //configurar o dado recebido
+        textNomeVinho.setText(nomeVinho);
+
     }
 }
