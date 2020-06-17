e7f80a2bedce09d89940c90e0d1050d32b736acf
Daniel Pessoto <d.pessoto@hotmail.com>
2020-06-17 20:44:04 -0300

    Adicionando descrição dos vinhos

--- app/src/main/java/pessoto/android/evino/activity/DetalhesActivity.java
+++ app/src/main/java/pessoto/android/evino/activity/DetalhesActivity.java
@@ -37,35 +37,35 @@ public class DetalhesActivity extends AppCompatActivity {
 
         switch (vinho) {
             case "Divin Marquis 2019":
-                textDescricao.setText("Teste");
+                textDescricao.setText(R.string.descricao_1);
                 imageVinho.setImageResource(R.drawable.divin_marquis_2019);
                 break;
             case "Fuenteviña Pinot Noir":
-                textDescricao.setText("Teste 2");
+                textDescricao.setText(R.string.descricao_2);
                 imageVinho.setImageResource(R.drawable.fuentevina_pinot_noir);
                 break;
             case "Fundo Las Lagunas Reserva Pinot Noir 2019":
-                textDescricao.setText("Teste");
+                textDescricao.setText(R.string.descricao_3);
                 imageVinho.setImageResource(R.drawable.fundo_las_lagunas_reserva_pinot_noir_2019);
                 break;
             case "Alto Vez Vinho Verde Branco DOC":
-                textDescricao.setText("Teste 2");
+                textDescricao.setText(R.string.descricao_4);
                 imageVinho.setImageResource(R.drawable.alto_vez_vinho_verde_vranco_doc);
                 break;
             case "Famiglia Castellani Chianti Riserva DOCG 2015":
-                textDescricao.setText("Teste");
+                textDescricao.setText(R.string.descricao_5);
                 imageVinho.setImageResource(R.drawable.famiglia_castellani_chianti_riserva_docg_2015);
                 break;
             case "Villa Pampini Vino Bianco":
-                textDescricao.setText("Teste 2");
+                textDescricao.setText(R.string.descricao_6);
                 imageVinho.setImageResource(R.drawable.villa_pampini_vino_bianco);
                 break;
             case "Primi Luis Gurpegui Gran Reserva Navarra D.O. 2007":
-                textDescricao.setText("Teste");
+                textDescricao.setText(R.string.descricao_7);
                 imageVinho.setImageResource(R.drawable.primi_luis_gurpegui_gran_reserva_navarra_do_2007);
                 break;
             case "Ajimez Vino Blanco 2018":
-                textDescricao.setText("Teste 2");
+                textDescricao.setText(R.string.descricao_8);
                 imageVinho.setImageResource(R.drawable.ajimez_vino_blanco_2018);
                 break;
 
