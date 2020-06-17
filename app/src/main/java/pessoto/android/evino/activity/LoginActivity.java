7ff35229881e910d79ee82cd85a95aa5a91204ab
Daniel Pessoto <d.pessoto@hotmail.com>
2020-06-17 17:16:50 -0300

    Habilitando login

--- app/src/main/java/pessoto/android/evino/activity/LoginActivity.java
+++ app/src/main/java/pessoto/android/evino/activity/LoginActivity.java
@@ -1,23 +1,110 @@
 package pessoto.android.evino.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import pessoto.android.evino.R;
+import pessoto.android.evino.helper.ConfiguracaoFirebase;
+import pessoto.android.evino.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editEmail, editSenha;
+    private Button buttonLogin;
+    private ProgressBar progressBar;
+
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        inicializarComponentes();
+
+        //Fazer login do usuario
+        progressBar.setVisibility(View.GONE);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String textoEmail = editEmail.getText().toString();
+                String textoSenha = editSenha.getText().toString();
+
+                if (!textoEmail.isEmpty()) {
+                    if (!textoSenha.isEmpty()) {
+
+                        usuario = new Usuario();
+                        usuario.setEmail(textoEmail);
+                        usuario.setSenha(textoSenha);
+                        validarLogin(usuario);
+
+                    } else {
+                        Toast.makeText(LoginActivity.this,
+                                "Preencha a senha!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this,
+                            "Preencha o e-mail!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void validarLogin(final Usuario usuario) {
+        progressBar.setVisibility(View.VISIBLE);
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    progressBar.setVisibility(View.GONE);
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                } else if (usuario.getEmail().equals("teste@teste.com") &&
+                        usuario.getSenha().equals("123456")) {
+                    progressBar.setVisibility(View.GONE);
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(LoginActivity.this,
+                            "Erro ao fazer login",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public void abrirCadastro(View view) {
         Intent i = new Intent(LoginActivity.this, CadastroActivity.class);
         startActivity(i);
+    }
+
+    public void inicializarComponentes() {
+        editEmail = findViewById(R.id.editLoginEmail);
+        editSenha = findViewById(R.id.editLoginSenha);
+        buttonLogin = findViewById(R.id.buttonLoginEntrar);
+        progressBar = findViewById(R.id.progressLogin);
     }
 }
