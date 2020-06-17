7fe8ce01ad566d63aac2084066e670c33e1b753c
Daniel Pessoto <d.pessoto@hotmail.com>
2020-06-17 18:01:02 -0300

    Criando uma listando e o metodo sair

--- app/src/main/java/pessoto/android/evino/activity/MainActivity.java
+++ app/src/main/java/pessoto/android/evino/activity/MainActivity.java
@@ -2,15 +2,50 @@ package pessoto.android.evino.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import pessoto.android.evino.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listVinhos;
+    private String[] vinhos = {
+            "Divin Marquis 2019", "Fuenteviña Pinot Noir",
+            "Fundo Las Lagunas Reserva Pinot Noir 2019", "Alto Vez Vinho Verde Branco DOC",
+            "Famiglia Castellani Chianti Riserva DOCG 2015", "Villa Pampini Vino Bianco",
+            "Primi Luis Gurpegui Gran Reserva Navarra D.O. 2007", "Ajimez Vino Blanco 2018",
+            "Divin Marquis 2019", "Fuenteviña Pinot Noir",
+            "Fundo Las Lagunas Reserva Pinot Noir 2019", "Alto Vez Vinho Verde Branco DOC",
+            "Famiglia Castellani Chianti Riserva DOCG 2015", "Villa Pampini Vino Bianco",
+            "Primi Luis Gurpegui Gran Reserva Navarra D.O. 2007", "Ajimez Vino Blanco 2018"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listVinhos = findViewById(R.id.listVinhos);
+
+        //Adaptador para a lista
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                vinhos
+        );
+
+        //Adicionar adaptador para a lista
+        listVinhos.setAdapter(adaptador);
+    }
+
+    public void sair(View view){
+        Intent i = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
