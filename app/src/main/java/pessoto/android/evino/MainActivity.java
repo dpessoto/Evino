8caf8bea0fc39c592dfea147e2338d97b4f9505c
Daniel Pessoto <d.pessoto@hotmail.com>
2020-06-17 11:54:53 -0300

    initial commit

---
+++ app/src/main/java/pessoto/android/evino/MainActivity.java
@@ -0,0 +1,14 @@
+package pessoto.android.evino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
