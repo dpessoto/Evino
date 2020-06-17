4aab9e77a585db9784c0c429177432863452e5a3
Daniel Pessoto <d.pessoto@hotmail.com>
2020-06-17 18:20:19 -0300

    Criando uma listando e o metodo sair

--- app/src/main/java/pessoto/android/evino/activity/DetalhesActivity.java
+++ app/src/main/java/pessoto/android/evino/activity/DetalhesActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import pessoto.android.evino.R;
+
 public class DetalhesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detalhes);
+        setContentView(R.layout.activity_login);
     }
 }
