7db6faf968f65f4bf6b5ff9d227b3c765049672d
Daniel Pessoto <d.pessoto@hotmail.com>
2020-06-18 21:01:43 -0300

    Alterações de layout

--- app/src/main/java/pessoto/android/evino/activity/SliderActivity.java
+++ app/src/main/java/pessoto/android/evino/activity/SliderActivity.java
@@ -1,24 +1,18 @@
 package pessoto.android.evino.activity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
-import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
-import com.heinrichreimersoftware.materialintro.slide.Slide;
 
 import pessoto.android.evino.R;
 
 public class SliderActivity extends IntroActivity {
 
-    @SuppressLint("ResourceType")
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_slider);
 
         setButtonBackVisible(false);
         setButtonNextVisible(false);
@@ -36,11 +30,6 @@ public class SliderActivity extends IntroActivity {
         addSlide(new FragmentSlide.Builder()
                 .background(android.R.color.holo_red_light)
                 .fragment(R.layout.intro_3)
-                .build()
-        );
-        addSlide(new FragmentSlide.Builder()
-                .background(android.R.color.holo_red_light)
-                .fragment(R.layout.intro_4)
                 .canGoForward(false)
                 .build()
         );
